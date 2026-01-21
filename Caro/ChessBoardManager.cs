74f9216bea560fa3b9ce8004a7407dbc5957059f
Bi9Chuot <Bi9Chuot@BI9CHUOT>
2026-01-21 12:14:39 +0700

    Add project files.

---
+++ Caro/ChessBoardManager.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Caro
+{
+    public class ChessBoardManager
+    {
+
+
+        #region Properties
+        private Panel chessBoard;
+
+        public Panel ChessBoard
+        {
+            get { return chessBoard; }
+            set { chessBoard = value; }
+        }
+
+        
+
+        private List<Player> player;
+        public List<Player> Player
+        {
+            get { return player; }
+            set { player = value; } 
+        }
+        private int currentPlayer;
+        public int CurrentPlayer
+        {
+            get { return currentPlayer; }
+            set { currentPlayer = value; }
+        }
+        private TextBox playerName;
+        public TextBox PlayerName
+        {
+            get { return playerName; }
+            set {  playerName = value; }
+        }
+        private PictureBox playerMark;
+        public PictureBox PlayerMark
+        {
+            get { return playerMark; }
+            set { playerMark = value; }
+        }
+
+        
+
+        private List<List<Button>> matrix;
+        public List<List<Button>> Matrix
+        {
+            get { return matrix; }
+            set { matrix = value; }
+        }
+        #endregion
+
+        #region Initialize
+
+        public ChessBoardManager(Panel chessBoard, TextBox playerName,PictureBox mark)
+        {
+            this.ChessBoard = chessBoard;
+            this.PlayerName = playerName;
+            this.PlayerMark = mark;
+            Player = new List<Player>() 
+            { 
+                new Player("Bi9Chuot",Image.FromFile("D:\\Laptrinhmang\\Caro\\Caro\\Resources\\o.png")),
+                new Player("Cuong7Nui",Image.FromFile("D:\\Laptrinhmang\\Caro\\Caro\\Resources\\x.png")),
+                
+            };
+            CurrentPlayer = 0;
+            ChangePlayer();
+        }
+
+        #endregion
+
+        #region Methods
+        public void DrawChessBoard()
+        {
+            Matrix = new List<List<Button>>();
+            Button oldButton = new Button() { Width = 0, Location = new Point(0, 0) };
+            for (int i = 0; i < Cons.CHESS_BOARD_HEIGHT; i++)
+            {
+                //Matrix.Add(new List<Button>());
+                for (int j = 0; j < Cons.CHESS_BOARD_WIDTH; j++)
+                {
+                    Button btn = new Button()
+                    {
+                        Width = Cons.CHESS_WIDTH,
+                        Height = Cons.CHESS_HEIGHT,
+                        Location = new Point(oldButton.Location.X + oldButton.Width, oldButton.Location.Y),
+                        BackgroundImageLayout = ImageLayout.Stretch
+                    };
+                    btn.Click += btn_Click; 
+                    ChessBoard.Controls.Add(btn);
+                    oldButton = btn;
+                }
+                oldButton.Location = new Point(0, oldButton.Location.Y + Cons.CHESS_HEIGHT);
+                oldButton.Width = 0;
+                oldButton.Height = 0;
+            }
+
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+            if (btn.BackgroundImage != null) 
+                return;
+
+
+            Mark(btn);
+            ChangePlayer();
+        }
+
+        private void Mark(Button btn)
+        {
+            btn.BackgroundImage = Player[CurrentPlayer].Mark;
+            CurrentPlayer = CurrentPlayer == 1 ? 0 : 1;
+        }
+
+        private void ChangePlayer()
+        {
+            PlayerName.Text = Player[CurrentPlayer].Name;
+            PlayerMark.Image = Player[CurrentPlayer].Mark;
+        }
+
+
+        #endregion
+
+    }
+}
---
+++ Caro/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿namespace Caro
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pnl_chessBoard = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.img_Player = new System.Windows.Forms.PictureBox();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.txt_PlayerName = new System.Windows.Forms.TextBox();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.img_Player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnl_chessBoard
+            // 
+            this.pnl_chessBoard.BackColor = System.Drawing.SystemColors.Control;
+            this.pnl_chessBoard.Location = new System.Drawing.Point(12, 12);
+            this.pnl_chessBoard.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pnl_chessBoard.Name = "pnl_chessBoard";
+            this.pnl_chessBoard.Size = new System.Drawing.Size(712, 518);
+            this.pnl_chessBoard.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Location = new System.Drawing.Point(729, 12);
+            this.panel2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(269, 270);
+            this.panel2.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.pictureBox1.Image = global::Caro.Properties.Resources.back_caro;
+            this.pictureBox1.Location = new System.Drawing.Point(4, 4);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(260, 262);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel3
+            // 
+            this.panel3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.textBox2);
+            this.panel3.Controls.Add(this.img_Player);
+            this.panel3.Controls.Add(this.progressBar1);
+            this.panel3.Controls.Add(this.txt_PlayerName);
+            this.panel3.Location = new System.Drawing.Point(729, 287);
+            this.panel3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(269, 244);
+            this.panel3.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Elephant", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(29, 177);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 37);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "5 in line to win";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(4, 126);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(140, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "LAN";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(4, 84);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(140, 22);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "127.0.0.1";
+            // 
+            // img_Player
+            // 
+            this.img_Player.BackColor = System.Drawing.SystemColors.Control;
+            this.img_Player.Location = new System.Drawing.Point(149, 4);
+            this.img_Player.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.img_Player.Name = "img_Player";
+            this.img_Player.Size = new System.Drawing.Size(116, 144);
+            this.img_Player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.img_Player.TabIndex = 2;
+            this.img_Player.TabStop = false;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(4, 44);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(140, 23);
+            this.progressBar1.TabIndex = 1;
+            // 
+            // txt_PlayerName
+            // 
+            this.txt_PlayerName.Location = new System.Drawing.Point(4, 4);
+            this.txt_PlayerName.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txt_PlayerName.Name = "txt_PlayerName";
+            this.txt_PlayerName.ReadOnly = true;
+            this.txt_PlayerName.Size = new System.Drawing.Size(140, 22);
+            this.txt_PlayerName.TabIndex = 0;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1005, 591);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.pnl_chessBoard);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Caro_game";
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.img_Player)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnl_chessBoard;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.PictureBox img_Player;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox txt_PlayerName;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
